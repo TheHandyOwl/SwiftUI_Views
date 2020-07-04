45a09b061721d87bb67211999074fbba848fbe42
The Handy Owl <thehandyowl.io@gmail.com>
2020-07-04 20:37:03 +0200

    New target: TabView ready

---
+++ SwiftUI_Views_TabView/Views/twitterView.swift
@@ -0,0 +1,21 @@
+//
+//  twitterView.swift
+//  SwiftUI_Views_TabView
+//
+//  Created by Carlos on 04/07/2020.
+//  Copyright © 2020 TestCompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct twitterView: View {
+    var body: some View {
+        Text("Twitter view!")
+    }
+}
+
+struct twitterView_Previews: PreviewProvider {
+    static var previews: some View {
+        twitterView()
+    }
+}
